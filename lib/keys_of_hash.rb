296a51b0d5ecfebf6ef564b2007f1111eb1059e7
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 03:05:53 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,25 @@
+def animals
+  animals = 
+   {"sugar glider"=>"Australia",
+   "aye-aye"=> "Madagascar",
+   "red-footed tortoise"=>"Panama",
+   "kangaroo"=> "Australia",
+   "tomato frog"=>"Madagascar",
+   "koala"=>"Australia"} 
+end 
+
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+    self.each do |key, value|
+    arguments.each do |i|
+      if i == value 
+        array.push(key)
+      end
+    end
   end
-end
+ return array
+ end
+ end
